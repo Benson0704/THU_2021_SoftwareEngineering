f6efa22558fe2539fed3b57c403c7f1c5d67efbc
brisa <zhaoxiyu18@mails.tsinghua.edu.cn>
2021-03-18 01:26:57 +0800

    修复了空格和缩进的格式问题

--- app/settings.py
+++ app/settings.py
@@ -91,19 +91,19 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': （'django.contrib.auth.password_validation.'
-        'UserAttributeSimilarityValidator'),
+            'UserAttributeSimilarityValidator'),
     },
     {
         'NAME': ('django.contrib.auth.password_validation.'
-        'MinimumLengthValidator'),
+            'MinimumLengthValidator'),
     },
     {
         'NAME': ('django.contrib.auth.password_validation.'
-        'CommonPasswordValidator'),
+            'CommonPasswordValidator'),
     },
     {
         'NAME': ('django.contrib.auth.password_validation.'
-        'NumericPasswordValidator'),
+            'NumericPasswordValidator'),
     },
 ]
 
@@ -111,18 +111,18 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE='en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE='UTC'
 
-USE_I18N = True
+USE_I18N=True
 
-USE_L10N = True
+USE_L10N=True
 
-USE_TZ = True
+USE_TZ =True
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL='/static/'
