5a8300f8aa3003b428e41e7c478c284281cd9a5e
brisa <zhaoxiyu18@mails.tsinghua.edu.cn>
2021-03-21 18:58:11 +0800

    add a output file to test whether .py work properly

--- testMysql.py
+++ testMysql.py
@@ -1,6 +1,10 @@
 import pymysql
 
 def insertData():
+    #输出文件作为测试
+    file= open('testMysql.txt','w')
+    file.write("Hello world!")
+    file.close()
 
     db = pymysql.connect(host="mysql.FullHouse.secoder.local",port=3306,user="root",passwd="kuaishou",db="kuaishou",charset='utf8')
 
