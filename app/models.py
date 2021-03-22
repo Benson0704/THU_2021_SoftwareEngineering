79f22f2a4dd803020fc9caab1f361d5bc3a36d90
brisa <zhaoxiyu18@mails.tsinghua.edu.cn>
2021-03-22 23:40:51 +0800

    fix whitespace missing error in models.py

--- app/models.py
+++ app/models.py
@@ -1,17 +1,17 @@
 from django.db import models
 
 class User(models.Model):
-    open_id = models.CharField(max_length=50,primary_key=True)
+    open_id = models.CharField(max_length=50, primary_key=True)
     public_count = models.IntegerField(default=0)
     friend_count = models.IntegerField(default=0)
     private_count = models.IntegerField(default=0)
     all_count = models.IntegerField(default=0)
 
 class Video(models.Model):
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     photo_id = models.CharField(max_length=50)
-    caption = models.CharField(max_length=500,default="NULL CAPTION")
-    cover = models.CharField(max_length=500,default="NULL COVER")
+    caption = models.CharField(max_length=500, default="NULL CAPTION")
+    cover = models.CharField(max_length=500, default="NULL COVER")
     play_url = models.CharField(max_length=500)
     create_time = models.DateTimeField()
     like_count = models.IntegerField(default=0)
