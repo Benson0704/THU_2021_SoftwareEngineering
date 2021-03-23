ff0786a3117007bc38917d5c3d22cb1a79e0cd22
Benson0704 <wang-bn19@mails.tsinghua.edu.cn>
2021-03-24 05:42:02 +0800

    feat: init the create user function

--- logIn/utils.py
+++ logIn/utils.py
@@ -1,4 +1,14 @@
 '''
 this module provides necessary functions and auxiliary functions
-WARNING: all fucntions that not used to handle frontend request DIRECTLY should write here
+WARNING:
+all fucntions not used to handle frontend request DIRECTLY should write here
 '''
+
+
+def create_user(video_list, count_dictionary):
+    '''
+    this function should create a user in User model
+    '''
+    '''
+    wait to finish
+    '''
