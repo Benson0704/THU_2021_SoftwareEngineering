37bc927fd9a5a7b94039c1337fa78b53c25e9e6c
Benson0704 <wang-bn19@mails.tsinghua.edu.cn>
2021-03-25 03:54:41 +0800

    fix: try to fix unit test error remote

--- tests/login_test.py
+++ tests/login_test.py
@@ -5,6 +5,7 @@ this file should be a .py file as tests for login
 import sys
 import os
 sys.path.append('..')
+os.system('export DJANGO_SETTINGS_MODULE=app.settings')
 from logIn.tests import test_logIn
 
 
