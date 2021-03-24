5692b8d5e06ea5b13f1bef9d4a67c74ccc3fcacb
Benson0704 <wang-bn19@mails.tsinghua.edu.cn>
2021-03-25 04:12:11 +0800

    fix: change imports ranks

--- tests/login_test.py
+++ tests/login_test.py
@@ -5,8 +5,8 @@ this file should be a .py file as tests for login
 import sys
 import os
 sys.path.append('..')
-from logIn.tests import test_logIn
 os.environ['DJANGO_SETTINGS_MODULE'] = 'app.settings'
+from logIn.tests import test_logIn
 
 
 def test():
