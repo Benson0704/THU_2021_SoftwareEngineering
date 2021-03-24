b15fa0839f9c643eeb4aa55352a9eb650855c715
brisa <zhaoxiyu18@mails.tsinghua.edu.cn>
2021-03-25 00:03:04 +0800

    correct mistakes in models and utils,add one more test function in test.py

--- logIn/tests.py
+++ logIn/tests.py
@@ -12,34 +12,34 @@ class TestLogin(unittest.TestCase):
     This is a unittest for User
     '''
     def setUp(self):
-        brisa = User.objects.create(_open_id="todayisagoodday",
-                                    _public_count=2,
-                                    _friend_count=0,
-                                    _private_count=0,
-                                    _all_count=2)
+        brisa = User.objects.create(open_id="todayisagoodday",
+                                    public_count=2,
+                                    friend_count=0,
+                                    private_count=0,
+                                    all_count=2)
         brisa.save()
 
-        new_video = Video.objects.create(_user=brisa,
-                                         _photo_id="this is a sunset photo in Hogwards",
-                                         _caption="hogwards sunset",
-                                         _cover="https://HogwardsSunset",
-                                         _play_url="https://PlayHogwardsSunset",
-                                         _create_time=20210307191817,
-                                         _like_count=2021,
-                                         _comment_count=74,
-                                         _view_count=200074,
-                                         _pending=False)
+        new_video = Video.objects.create(user=brisa.open_id,
+                                         photo_id="this is a sunset photo in Hogwards",
+                                         caption="hogwards sunset",
+                                         cover="https://HogwardsSunset",
+                                         play_url="https://PlayHogwardsSunset",
+                                         create_time=20210307191817,
+                                         like_count=2021,
+                                         comment_count=74,
+                                         view_count=200074,
+                                         pending=False)
         new_video.save()
-        new_video = Video.objects.create(_user=brisa,
-                                         _photo_id="this is a photo on Mars",
-                                         _caption="Mars view",
-                                         _cover="https://MarsView",
-                                         _play_url="https://PlayMarsView",
-                                         _create_time=20210307191817,
-                                         _like_count=2021,
-                                         _comment_count=74,
-                                         _view_count=200074,
-                                         _pending=False)
+        new_video = Video.objects.create(user=brisa.open_id,
+                                         photo_id="this is a photo on Mars",
+                                         caption="Mars view",
+                                         cover="https://MarsView",
+                                         play_url="https://PlayMarsView",
+                                         create_time=20210307191817,
+                                         like_count=2021,
+                                         comment_count=74,
+                                         view_count=200074,
+                                         pending=False)
         new_video.save()
 
 
@@ -66,37 +66,38 @@ class TestLogin(unittest.TestCase):
         }
 
         utils.initialize_new_user(open_id=open_id, video_list=video_list, count_dictionary=count_dictionary)
-        self.assertTrue(User.objects.filter(_open_id=open_id).exists())
-        self.assertTrue(Video.objects.filter(_photo_id="Test photo_id").exists())
+        self.assertTrue(User.objects.filter(open_id=open_id).exists())
+        self.assertTrue(Video.objects.filter(photo_id="Test photo_id").exists())
 
 
     def test_get_registered_user(self):
         res_video_list, res_count_dictionary = utils.get_registered_user("todayisagoodday")
         expected_video_list = [
             {
-                '_photo_id': "this is a sunset photo in Hogwards",
-                '_caption': "hogwards sunset",
-                '_cover': "https://HogwardsSunset",
-                '_play_url': "https://PlayHogwardsSunset",
-                '_create_time': 20210307191817,
-                '_like_count': 2021,
-                '_comment_count': 74,
-                '_view_count': 200074,
-                '_pending': False
+                'photo_id': "this is a sunset photo in Hogwards",
+                'caption': "hogwards sunset",
+                'cover': "https://HogwardsSunset",
+                'play_url': "https://PlayHogwardsSunset",
+                'create_time': 20210307191817,
+                'like_count': 2021,
+                'comment_count': 74,
+                'view_count': 200074,
+                'pending': False
             },
             {
-                '_photo_id': "this is a photo on Mars",
-                '_caption': "Mars view",
-                '_cover': "https://MarsView",
-                '_play_url': "https://PlayMarsView",
-                '_create_time': 20210307191817,
-                '_like_count': 2021,
-                '_comment_count': 74,
-                '_view_count': 200074,
-                '_pending': False
+                'photo_id': "this is a photo on Mars",
+                'caption': "Mars view",
+                'cover': "https://MarsView",
+                'play_url': "https://PlayMarsView",
+                'create_time': 20210307191817,
+                'like_count': 2021,
+                'comment_count': 74,
+                'view_count': 200074,
+                'pending': False
             },
         ]
         expected_count_dictionary = {
+            'open_id' : "todayisagoodday",
             'public_count' : 2,
             'friend_count' : 0,
             'private_count' : 0,
@@ -110,26 +111,26 @@ class TestLogin(unittest.TestCase):
         open_id = "todayisagoodday"
         video_list = [
             {
-                '_photo_id': "this is a sunset photo in Hogwards",
-                '_caption': "hogwards sunset",
-                '_cover': "https://HogwardsSunset",
-                '_play_url': "https://PlayHogwardsSunset",
-                '_create_time': 20210307191817,
-                '_like_count': 2021,
-                '_comment_count': 74,
-                '_view_count': 200074,
-                '_pending': False
+                'photo_id': "this is a sunset photo in Hogwards",
+                'caption': "hogwards sunset",
+                'cover': "https://HogwardsSunset",
+                'play_url': "https://PlayHogwardsSunset",
+                'create_time': 20210307191817,
+                'like_count': 2021,
+                'comment_count': 74,
+                'view_count': 200074,
+                'pending': False
             },
             {
-                '_photo_id': "this is a dog in New Zealand",
-                '_caption': "New Zealand dog",
-                '_cover': "https://NewZealanddog",
-                '_play_url': "https://PlayNewZealanddog",
-                '_create_time': 20210307191817,
-                '_like_count': 2021,
-                '_comment_count': 74,
-                '_view_count': 200074,
-                '_pending': False
+                'photo_id': "this is a dog in New Zealand",
+                'caption': "New Zealand dog",
+                'cover': "https://NewZealanddog",
+                'play_url': "https://PlayNewZealanddog",
+                'create_time': 20210307191817,
+                'like_count': 2021,
+                'comment_count': 74,
+                'view_count': 200074,
+                'pending': False
             }
         ]
         count_dictionary = {
@@ -139,12 +140,16 @@ class TestLogin(unittest.TestCase):
             'all_count' : 2
         }
         utils.update_registered_user(open_id="todayisagoodday", video_list=video_list, count_dictionary=count_dictionary)
-        self.assertFalse(Video.objects.filter(_photo_id="this is a photo on Mars").exists())
-        self.assertTrue(Video.objects.filter(_photo_id="this is a dog in New Zealand").exists())
+        self.assertFalse(Video.objects.filter(photo_id="this is a photo on Mars").exists())
+        self.assertTrue(Video.objects.filter(photo_id="this is a dog in New Zealand").exists())
+
+
+    def test_is_registered(self):
+        self.assertTrue(utils.is_registered("todayisagoodday")
 
 
     def tearDown(self):
-        User.objects.filter(_open_id="todayisagoodday").delete()
+        User.objects.filter(open_id="todayisagoodday").delete()
 
 
 
--- logIn/utils.py
+++ logIn/utils.py
@@ -14,7 +14,7 @@ def is_registered(open_id):
     return: 1:yes 0:no
     3.24: untest
     '''
-    user = User.objects.filter(_open_id=open_id)
+    user = User.objects.filter(open_id=open_id)
     return bool(user)
 
 
@@ -24,27 +24,27 @@ def get_registered_user(open_id):
     return: list, dic
     3.24: untest
     '''
-    user = User.objects.get(_open_id=open_id)
-    video_list = Video.objects.filter(_user=user)
+    user = User.objects.get(open_id=open_id)
+    video_list = Video.objects.filter(user=user.open_id)
     res_video_list = []
     for video in video_list:
         video_dictionary = {}
-        video_dictionary['photo_id'] = video._photo_id
-        video_dictionary['caption'] = video._caption
-        video_dictionary['cover'] = video._cover
-        video_dictionary['play_url'] = video._play_url
-        video_dictionary['create_time'] = video._create_time
-        video_dictionary['like_count'] = video._like_count
-        video_dictionary['comment_count'] = video._comment_count
-        video_dictionary['view_count'] = video._view_count
-        video_dictionary['pending'] = video._pending
+        video_dictionary['photo_id'] = video.photo_id
+        video_dictionary['caption'] = video.caption
+        video_dictionary['cover'] = video.cover
+        video_dictionary['play_url'] = video.play_url
+        video_dictionary['create_time'] = video.create_time
+        video_dictionary['like_count'] = video.like_count
+        video_dictionary['comment_count'] = video.comment_count
+        video_dictionary['view_count'] = video.view_count
+        video_dictionary['pending'] = video.pending
         res_video_list.append(video_dictionary)
     res_count_dictionary = {}
     res_count_dictionary['open_id'] = open_id
-    res_count_dictionary['public_count'] = user._public_count
-    res_count_dictionary['friend_count'] = user._friend_count
-    res_count_dictionary['private_count'] = user._private_count
-    res_count_dictionary['all_count'] = user._all_count
+    res_count_dictionary['public_count'] = user.public_count
+    res_count_dictionary['friend_count'] = user.friend_count
+    res_count_dictionary['private_count'] = user.private_count
+    res_count_dictionary['all_count'] = user.all_count
     return res_video_list, res_count_dictionary
 
 
@@ -53,30 +53,30 @@ def update_registered_user(open_id, video_list, count_dictionary):
     this fuction should update a registered user
     3.24: untest
     '''
-    user = User.objects.get(_open_id=open_id)
-    user._public_count = count_dictionary['public_count']
-    user._friend_count = count_dictionary['friend_count']
-    user._private_count = count_dictionary['private_count']
-    user._all_count = count_dictionary['all_count']
+    user = User.objects.get(open_id=open_id)
+    user.public_count = count_dictionary['public_count']
+    user.friend_count = count_dictionary['friend_count']
+    user.private_count = count_dictionary['private_count']
+    user.all_count = count_dictionary['all_count']
     new_video_list = []
-    old_video_list = Video.objects.filter(_user=user)
+    old_video_list = Video.objects.filter(user=user.open_id)
     for video in video_list:
         new_video_list.append(video['photo_id'])
     for video in old_video_list:
-        if str(video._photo_id) not in new_video_list:
+        if str(video.photo_id) not in new_video_list:
             video.delete()
     for i, _ in enumerate(new_video_list):
-        if not bool(Video.objects.filter(_photo_id=new_video_list[i])):
-            video = Video(_user=user,
-                          _photo_id=video_list[i]['photo_id'],
-                          _caption=video_list[i]['caption'],
-                          _cover=video_list[i]['cover'],
-                          _play_url=video_list[i]['play_url'],
-                          _create_time=video_list[i]['create_time'].strftime(("%Y-%m-%d %H:%M:%S")),
-                          _like_count=video_list[i]['like_count'],
-                          _comment_count=video_list[i]['comment_count'],
-                          _view_count=video_list[i]['view_count'],
-                          _pending=video_list[i]['pending'])
+        if not bool(Video.objects.filter(photo_id=new_video_list[i])):
+            video = Video(user=user.open_id,
+                          photo_id=video_list[i]['photo_id'],
+                          caption=video_list[i]['caption'],
+                          cover=video_list[i]['cover'],
+                          play_url=video_list[i]['play_url'],
+                          create_time=video_list[i]['create_time'].strftime(("%Y-%m-%d %H:%M:%S")),
+                          like_count=video_list[i]['like_count'],
+                          comment_count=video_list[i]['comment_count'],
+                          view_count=video_list[i]['view_count'],
+                          pending=video_list[i]['pending'])
             video.save()
 
 
@@ -85,21 +85,21 @@ def initialize_new_user(open_id, video_list, count_dictionary):
     this function should create a user in User model and his works in Video
     3.24: untest
     '''
-    new_user = User(_open_id=open_id,
-                    _public_count=count_dictionary['public_count'],
-                    _friend_count=count_dictionary['friend_count'],
-                    _private_count=count_dictionary['private_count'],
-                    _all_count=count_dictionary['all_count'])
+    new_user = User(open_id=open_id,
+                    public_count=count_dictionary['public_count'],
+                    friend_count=count_dictionary['friend_count'],
+                    private_count=count_dictionary['private_count'],
+                    all_count=count_dictionary['all_count'])
     new_user.save()
     for video in video_list:
-        new_video = Video(_user=new_user,
-                          _photo_id=video['photo_id'],
-                          _caption=video['caption'],
-                          _cover=['cover'],
-                          _play_url=['play_url'],
-                          _create_time=video['create_time'],
-                          _like_count=video['like_count'],
-                          _comment_count=video['comment_count'],
-                          _view_count=video['view_count'],
-                          _pending=video['pending'])
+        new_video = Video(user=new_user.open_id,
+                          photo_id=video['photo_id'],
+                          caption=video['caption'],
+                          cover=['cover'],
+                          play_url=['play_url'],
+                          create_time=video['create_time'],
+                          like_count=video['like_count'],
+                          comment_count=video['comment_count'],
+                          view_count=video['view_count'],
+                          pending=video['pending'])
         new_video.save()
