df089bfd13b5db9eade5f1cce181fb4befcad99f
Benson0704 <wang-bn19@mails.tsinghua.edu.cn>
2021-03-25 01:34:38 +0800

    fix: try to fix unit test error1.0

--- logIn/tests.py
+++ logIn/tests.py
@@ -2,7 +2,7 @@
 this file should be a .py file as tests for login
 '''
 import utils
-from app.models import User,Video
+from app.models import User, Video
 import unittest
 from datetime import datetime
 
@@ -19,16 +19,17 @@ class TestLogin(unittest.TestCase):
                                     all_count=2)
         brisa.save()
 
-        new_video = Video.objects.create(user=brisa.open_id,
-                                         photo_id="this is a sunset photo in Hogwards",
-                                         caption="hogwards sunset",
-                                         cover="https://HogwardsSunset",
-                                         play_url="https://PlayHogwardsSunset",
-                                         create_time=20210307191817,
-                                         like_count=2021,
-                                         comment_count=74,
-                                         view_count=200074,
-                                         pending=False)
+        new_video = Video.objects.create(
+            user=brisa.open_id,
+            photo_id="this is a sunset photo in Hogwards",
+            caption="hogwards sunset",
+            cover="https://HogwardsSunset",
+            play_url="https://PlayHogwardsSunset",
+            create_time=20210307191817,
+            like_count=2021,
+            comment_count=74,
+            view_count=200074,
+            pending=False)
         new_video.save()
         new_video = Video.objects.create(user=brisa.open_id,
                                          photo_id="this is a photo on Mars",
@@ -41,7 +42,6 @@ class TestLogin(unittest.TestCase):
                                          view_count=200074,
                                          pending=False)
         new_video.save()
-
 
     def test_initialize_new_user(self):
         open_id = "Test open_id"
@@ -59,19 +59,22 @@ class TestLogin(unittest.TestCase):
         }
         video_list.append(Video)
         count_dictionary = {
-            'public_count' : 1,
-            'friend_count' : 0,
-            'private_count' : 0,
-            'all_count' : 1
+            'public_count': 1,
+            'friend_count': 0,
+            'private_count': 0,
+            'all_count': 1
         }
 
-        utils.initialize_new_user(open_id=open_id, video_list=video_list, count_dictionary=count_dictionary)
+        utils.initialize_new_user(open_id=open_id,
+                                  video_list=video_list,
+                                  count_dictionary=count_dictionary)
         self.assertTrue(User.objects.filter(open_id=open_id).exists())
-        self.assertTrue(Video.objects.filter(photo_id="Test photo_id").exists())
-
+        self.assertTrue(
+            Video.objects.filter(photo_id="Test photo_id").exists())
 
     def test_get_registered_user(self):
-        res_video_list, res_count_dictionary = utils.get_registered_user("todayisagoodday")
+        res_video_list, res_count_dictionary = utils.get_registered_user(
+            "todayisagoodday")
         expected_video_list = [
             {
                 'photo_id': "this is a sunset photo in Hogwards",
@@ -97,61 +100,59 @@ class TestLogin(unittest.TestCase):
             },
         ]
         expected_count_dictionary = {
-            'open_id' : "todayisagoodday",
-            'public_count' : 2,
-            'friend_count' : 0,
-            'private_count' : 0,
-            'all_count' : 2
+            'open_id': "todayisagoodday",
+            'public_count': 2,
+            'friend_count': 0,
+            'private_count': 0,
+            'all_count': 2
         }
         self.assertEqual(res_video_list, expected_video_list)
         self.assertEqual(res_count_dictionary, expected_count_dictionary)
 
-    
     def test_update_registered_user(self):
         open_id = "todayisagoodday"
-        video_list = [
-            {
-                'photo_id': "this is a sunset photo in Hogwards",
-                'caption': "hogwards sunset",
-                'cover': "https://HogwardsSunset",
-                'play_url': "https://PlayHogwardsSunset",
-                'create_time': 20210307191817,
-                'like_count': 2021,
-                'comment_count': 74,
-                'view_count': 200074,
-                'pending': False
-            },
-            {
-                'photo_id': "this is a dog in New Zealand",
-                'caption': "New Zealand dog",
-                'cover': "https://NewZealanddog",
-                'play_url': "https://PlayNewZealanddog",
-                'create_time': 20210307191817,
-                'like_count': 2021,
-                'comment_count': 74,
-                'view_count': 200074,
-                'pending': False
-            }
-        ]
+        video_list = [{
+            'photo_id': "this is a sunset photo in Hogwards",
+            'caption': "hogwards sunset",
+            'cover': "https://HogwardsSunset",
+            'play_url': "https://PlayHogwardsSunset",
+            'create_time': 20210307191817,
+            'like_count': 2021,
+            'comment_count': 74,
+            'view_count': 200074,
+            'pending': False
+        }, {
+            'photo_id': "this is a dog in New Zealand",
+            'caption': "New Zealand dog",
+            'cover': "https://NewZealanddog",
+            'play_url': "https://PlayNewZealanddog",
+            'create_time': 20210307191817,
+            'like_count': 2021,
+            'comment_count': 74,
+            'view_count': 200074,
+            'pending': False
+        }]
         count_dictionary = {
-            'public_count' : 2,
-            'friend_count' : 0,
-            'private_count' : 0,
-            'all_count' : 2
+            'public_count': 2,
+            'friend_count': 0,
+            'private_count': 0,
+            'all_count': 2
         }
-        utils.update_registered_user(open_id="todayisagoodday", video_list=video_list, count_dictionary=count_dictionary)
-        self.assertFalse(Video.objects.filter(photo_id="this is a photo on Mars").exists())
-        self.assertTrue(Video.objects.filter(photo_id="this is a dog in New Zealand").exists())
-
+        utils.update_registered_user(open_id="todayisagoodday",
+                                     video_list=video_list,
+                                     count_dictionary=count_dictionary)
+        self.assertFalse(
+            Video.objects.filter(photo_id="this is a photo on Mars").exists())
+        self.assertTrue(
+            Video.objects.filter(
+                photo_id="this is a dog in New Zealand").exists())
 
     def test_is_registered(self):
-        self.assertTrue(utils.is_registered("todayisagoodday")
-
+        self.assertTrue(utils.is_registered("todayisagoodday"))
 
     def tearDown(self):
         User.objects.filter(open_id="todayisagoodday").delete()
 
 
-
-if __name__ == '__main__':
+def test_logIn():
     unittest.main()
