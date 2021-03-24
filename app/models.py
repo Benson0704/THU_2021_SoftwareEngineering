42e3012eb4387b0d9b35a58b26e5acc7d690055f
Benson0704 <wang-bn19@mails.tsinghua.edu.cn>
2021-03-25 02:59:16 +0800

    fix: try to fix unit test 11.0

--- app/models.py
+++ app/models.py
@@ -1,31 +1,35 @@
 '''
 这个文件定义了后端的两个类
 '''
-from django.db import models
+import django.db
 
 
-class User(models.Model):
+class User(django.db.models.Model):
     '''
     用户类
     '''
-    open_id = models.CharField(max_length=50, primary_key=True)  # 用户id，用户唯一标志
-    public_count = models.IntegerField(default=0)  # 公开视频数量
-    friend_count = models.IntegerField(default=0)  # 仅好友可见的视频数量
-    private_count = models.IntegerField(default=0)  # 仅自己可见的视频数量
-    all_count = models.IntegerField(default=0)  # 视频总量
+    open_id = django.db.models.CharField(max_length=50,
+                                         primary_key=True)  # 用户id，用户唯一标志
+    public_count = django.db.models.IntegerField(default=0)  # 公开视频数量
+    friend_count = django.db.models.IntegerField(default=0)  # 仅好友可见的视频数量
+    private_count = django.db.models.IntegerField(default=0)  # 仅自己可见的视频数量
+    all_count = django.db.models.IntegerField(default=0)  # 视频总量
 
 
-class Video(models.Model):
+class Video(django.db.models.Model):
     '''
     视频类
     '''
-    user = models.ForeignKey(User.open_id, on_delete=models.CASCADE)  # 用户
-    photo_id = models.CharField(max_length=50)  # 作品id
-    caption = models.CharField(max_length=500, default="NULL CAPTION")  # 作品标题
-    cover = models.CharField(max_length=500, default="NULL COVER")  # 作品封面
-    play_url = models.CharField(max_length=500)  # 作品播放链接
-    create_time = models.DateTimeField(default=0)  # 作品创建时间
-    like_count = models.IntegerField(default=0)  # 作品点赞数
-    comment_count = models.IntegerField(default=0)  # 作品评论数
-    view_count = models.IntegerField(default=0)  # 作品观看数
-    pending = models.BooleanField()  # 作品状态（是否正在处理，不能观看）
+    user = django.db.models.ForeignKey(
+        User.open_id, on_delete=django.db.models.CASCADE)  # 用户
+    photo_id = django.db.models.CharField(max_length=50)  # 作品id
+    caption = django.db.models.CharField(max_length=500,
+                                         default="NULL CAPTION")  # 作品标题
+    cover = django.db.models.CharField(max_length=500,
+                                       default="NULL COVER")  # 作品封面
+    play_url = django.db.models.CharField(max_length=500)  # 作品播放链接
+    create_time = django.db.models.DateTimeField(default=0)  # 作品创建时间
+    like_count = django.db.models.IntegerField(default=0)  # 作品点赞数
+    comment_count = django.db.models.IntegerField(default=0)  # 作品评论数
+    view_count = django.db.models.IntegerField(default=0)  # 作品观看数
+    pending = django.db.models.BooleanField()  # 作品状态（是否正在处理，不能观看）
