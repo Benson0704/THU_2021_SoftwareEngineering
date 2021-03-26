4920a88f62362ea5cef83564ab08d3e531b2dd0f
Benson0704 <wang-bn19@mails.tsinghua.edu.cn>
2021-03-27 05:56:25 +0800

    test: set timezone=UTC for each test

--- app/times.py
+++ app/times.py
@@ -11,7 +11,6 @@ import time
 import datetime
 import os
 STANDARD_DATE = "%Y-%m-%d %H:%M:%S"
-os.environ['TZ'] = 'UTC'
 
 
 def datetime2string(_datetime):
@@ -19,6 +18,7 @@ def datetime2string(_datetime):
     receive: a datetime.datetime object
     return: a string like yyyy-mm-dd hh:mm:ss
     '''
+    os.environ['TZ'] = 'UTC'
     return _datetime.strftime(STANDARD_DATE)
 
 
@@ -27,6 +27,7 @@ def datetime2timestamp(_datetime):
     receive: a datetime.datetime object
     return: timestamp length=10
     '''
+    os.environ['TZ'] = 'UTC'
     return time.mktime(
         time.strptime(_datetime.strftime(STANDARD_DATE), STANDARD_DATE))
 
@@ -36,6 +37,7 @@ def string2timestamp(_string):
     receive: a string like yyyy-mm-dd hh:mm:ss
     return: timestamp length=10
     '''
+    os.environ['TZ'] = 'UTC'
     return time.mktime(time.strptime(_string, STANDARD_DATE))
 
 
@@ -44,6 +46,7 @@ def string2datetime(_string):
     receive: a string like yyyy-mm-dd hh:mm:ss
     return: a datetime.datetime object
     '''
+    os.environ['TZ'] = 'UTC'
     return datetime.datetime.strptime(_string, STANDARD_DATE)
 
 
@@ -52,6 +55,7 @@ def timestamp2datetime(_timestamp):
     receive: timestamp length=10
     return: a datetime.datetime object
     '''
+    os.environ['TZ'] = 'UTC'
     while _timestamp > 9999999999:
         _timestamp = _timestamp // 10
     return datetime.datetime.strptime(
@@ -64,6 +68,7 @@ def timestamp2string(_timestamp):
     receive: timestamp length=10
     return: a string like yyyy-mm-dd hh:mm:ss
     '''
+    os.environ['TZ'] = 'UTC'
     while _timestamp > 9999999999:
         _timestamp = _timestamp // 10
     return time.strftime(STANDARD_DATE, time.localtime(_timestamp))
