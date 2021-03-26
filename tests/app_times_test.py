42364c241d269ebd41cc6bd12a4d845fcb00361c
Benson0704 <wang-bn19@mails.tsinghua.edu.cn>
2021-03-27 05:29:19 +0800

    test: change assert function to catch error more clearly

--- tests/app_times_test.py
+++ tests/app_times_test.py
@@ -43,7 +43,7 @@ class TestAppTimes(unittest.TestCase):
             'string':
             app.times.datetime2string(self.test_result[1]['datetime'])
         }]
-        self.assertTrue(function_result == self.test_result)
+        self.assertEqual(function_result, self.test_result)
 
     def test_datetime2timestamp(self):
         '''
@@ -51,7 +51,7 @@ class TestAppTimes(unittest.TestCase):
         '''
         function_result = [{
             'timestamp':
-            app.times.datetime2string(self.test_result[0]['datetime']),
+            app.times.datetime2timestamp(self.test_result[0]['datetime']),
             'datetime':
             datetime(1970, 1, 1, 0, 0, 0),
             'string':
@@ -64,7 +64,7 @@ class TestAppTimes(unittest.TestCase):
             'string':
             '2181-04-04 11:51:06'
         }]
-        self.assertTrue(function_result == self.test_result)
+        self.assertEqual(function_result, self.test_result)
 
     def test_string2datetime(self):
         '''
@@ -75,7 +75,7 @@ class TestAppTimes(unittest.TestCase):
             self.test_result[0]['string'])
         function_result[1]['datetime'] = app.times.string2datetime(
             self.test_result[1]['string'])
-        self.assertTrue(function_result == self.test_result)
+        self.assertEqual(function_result, self.test_result)
 
     def test_string2timestamp(self):
         '''
@@ -86,7 +86,7 @@ class TestAppTimes(unittest.TestCase):
             self.test_result[0]['string'])
         function_result[1]['timestamp'] = app.times.string2timestamp(
             self.test_result[1]['string'])
-        self.assertTrue(function_result == self.test_result)
+        self.assertEqual(function_result, self.test_result)
 
     def test_timestamp2datetime(self):
         '''
@@ -97,7 +97,7 @@ class TestAppTimes(unittest.TestCase):
             self.test_result[0]['timestamp'])
         function_result[1]['datetime'] = app.times.timestamp2datetime(
             self.test_result[1]['timestamp'])
-        self.assertTrue(function_result == self.test_result)
+        self.assertEqual(function_result, self.test_result)
 
     def test_timestamp2string(self):
         '''
@@ -108,7 +108,7 @@ class TestAppTimes(unittest.TestCase):
             self.test_result[0]['timestamp'])
         function_result[1]['string'] = app.times.timestamp2string(
             self.test_result[1]['timestamp'])
-        self.assertTrue(function_result == self.test_result)
+        self.assertEqual(function_result, self.test_result)
 
     def tearDown(self):
         '''
