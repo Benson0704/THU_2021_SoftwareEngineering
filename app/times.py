f2ba810999f698b85e45f67b26327ff66a809e3e
Benson0704 <wang-bn19@mails.tsinghua.edu.cn>
2021-03-27 05:54:05 +0800

    style: fix code smell bugs

--- app/times.py
+++ app/times.py
@@ -10,6 +10,8 @@ WARNING:
 import time
 import datetime
 import os
+STANDARD_DATE = "%Y-%m-%d %H:%M:%S"
+os.environ['TZ'] = 'UTC'
 
 
 def datetime2string(_datetime):
@@ -17,8 +19,7 @@ def datetime2string(_datetime):
     receive: a datetime.datetime object
     return: a string like yyyy-mm-dd hh:mm:ss
     '''
-    os.environ['TZ'] = 'UTC'
-    return _datetime.strftime("%Y-%m-%d %H:%M:%S")
+    return _datetime.strftime(STANDARD_DATE)
 
 
 def datetime2timestamp(_datetime):
@@ -26,10 +27,8 @@ def datetime2timestamp(_datetime):
     receive: a datetime.datetime object
     return: timestamp length=10
     '''
-    os.environ['TZ'] = 'UTC'
     return time.mktime(
-        time.strptime(_datetime.strftime("%Y-%m-%d %H:%M:%S"),
-                      '%Y-%m-%d %H:%M:%S'))
+        time.strptime(_datetime.strftime(STANDARD_DATE), STANDARD_DATE))
 
 
 def string2timestamp(_string):
@@ -37,8 +36,7 @@ def string2timestamp(_string):
     receive: a string like yyyy-mm-dd hh:mm:ss
     return: timestamp length=10
     '''
-    os.environ['TZ'] = 'UTC'
-    return time.mktime(time.strptime(_string, '%Y-%m-%d %H:%M:%S'))
+    return time.mktime(time.strptime(_string, STANDARD_DATE))
 
 
 def string2datetime(_string):
@@ -46,8 +44,7 @@ def string2datetime(_string):
     receive: a string like yyyy-mm-dd hh:mm:ss
     return: a datetime.datetime object
     '''
-    os.environ['TZ'] = 'UTC'
-    return datetime.datetime.strptime(_string, '%Y-%m-%d %H:%M:%S')
+    return datetime.datetime.strptime(_string, STANDARD_DATE)
 
 
 def timestamp2datetime(_timestamp):
@@ -55,12 +52,11 @@ def timestamp2datetime(_timestamp):
     receive: timestamp length=10
     return: a datetime.datetime object
     '''
-    os.environ['TZ'] = 'UTC'
     while _timestamp > 9999999999:
         _timestamp = _timestamp // 10
     return datetime.datetime.strptime(
-        time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(_timestamp)),
-        '%Y-%m-%d %H:%M:%S')
+        time.strftime(STANDARD_DATE, time.localtime(_timestamp)),
+        STANDARD_DATE)
 
 
 def timestamp2string(_timestamp):
@@ -68,16 +64,6 @@ def timestamp2string(_timestamp):
     receive: timestamp length=10
     return: a string like yyyy-mm-dd hh:mm:ss
     '''
-    os.environ['TZ'] = 'UTC'
     while _timestamp > 9999999999:
         _timestamp = _timestamp // 10
-    return time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(_timestamp))
-
-
-if __name__ == '__main__':  # test
-    print(datetime2timestamp(datetime.datetime(1970, 1, 1, 0, 0, 0)))
-    print(datetime2string(datetime.datetime(1970, 1, 1, 0, 0, 0)))
-    print(string2datetime("1970-01-01 00:00:00"))
-    print(string2timestamp("1970-01-01 00:00:00"))
-    print(timestamp2datetime(00000000000))
-    print(timestamp2string(0000000000))
+    return time.strftime(STANDARD_DATE, time.localtime(_timestamp))
