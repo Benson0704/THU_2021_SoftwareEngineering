a771dd88d30c38684cea2416d91d1428f65bb06a
brisa <zhaoxiyu18@mails.tsinghua.edu.cn>
2021-03-28 13:06:05 +0800

    delete useless codes in models.py

--- app/models.py
+++ app/models.py
@@ -5,7 +5,7 @@
 from django.db import models
 import os
 import django
-os.environ['DJANGO_SETTINGS_MODULE'] = 'app.settings'
+# os.environ['DJANGO_SETTINGS_MODULE'] = 'app.settings'
 # django.setup()
 
 
