a49fd2c050e96375bbcfd6b94f4746ec72877bf4
brisa <zhaoxiyu18@mails.tsinghua.edu.cn>
2021-03-30 12:09:12 +0800

    add migrate function, rebuild kuaishou database

--- config/run.sh
+++ config/run.sh
@@ -1,5 +1,5 @@
 # add some others
 export DJANGO_SETTINGS_MODULE=app.settings
-# python manage.py makemigrations app
-# python manage.py migrate app
+python manage.py makemigrations app
+python manage.py migrate app
 python manage.py runserver localhost:80
