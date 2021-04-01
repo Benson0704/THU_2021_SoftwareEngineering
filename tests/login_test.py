c0f9bb31249e9633a93f3a9609d5bb2b741e45bf
弋紫轩 <2019011307@secoder.net>
2021-04-01 16:36:15 +0800

    test: test the emoji in unit-test on secoder database

--- tests/login_test.py
+++ tests/login_test.py
@@ -54,7 +54,7 @@ class TestLogin(unittest.TestCase):
         Video.objects.filter(photo_id='this is a photo on Mars').delete()
         new_video = Video.objects.create(user=brisa,
                                          photo_id="this is a photo on Mars",
-                                         caption="Mars view",
+                                         caption="Mars view🔥",
                                          cover="https://MarsView",
                                          play_url="https://PlayMarsView",
                                          create_time='2021-03-07 12:13:14',
@@ -137,7 +137,7 @@ class TestLogin(unittest.TestCase):
                 'photo_id':
                 "this is a photo on Mars",
                 'caption':
-                "Mars view",
+                "Mars view🔥",
                 'cover':
                 "https://MarsView",
                 'play_url':
