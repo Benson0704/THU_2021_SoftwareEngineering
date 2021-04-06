924289391fe867dbe86000e8b1b8a51b4e1a8b6b
Benson0704 <wang-bn19@mails.tsinghua.edu.cn>
2021-04-07 01:58:56 +0800

    fix: delete invaild syntax

--- manageWorks/views.py
+++ manageWorks/views.py
@@ -90,8 +90,7 @@ def get_label_list(request):
                 })
             return app.utils.gen_response(200, return_list)
         except:
-            return app.utils.gen_response(
-                400, 'json content error'）
+            return app.utils.gen_response(400, 'json content error')
     elif request.method == 'POST':
         ret = request.body
         try:
