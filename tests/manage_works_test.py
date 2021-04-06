41257d7be80b5fe1db3f6d99ae77e0fc1f1c5245
Benson0704 <wang-bn19@mails.tsinghua.edu.cn>
2021-04-07 05:31:46 +0800

    fix: try to fix test

--- tests/manage_works_test.py
+++ tests/manage_works_test.py
@@ -120,8 +120,7 @@ class TestManageWorks(TestCase):
         self.assertEqual(response['data'], expected_vedioslists)
 
     def test_get_label_list_get_openid_lost(self):
-        response = self.client.get('/api/video/label',
-                                   content_type="application/json")
+        response = self.client.get('/api/video/label')
         self.assertEqual(400, response.json()['code'])
 
     def test_get_label_list_post_openid_lost(self):
