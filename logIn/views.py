77149b65e62034bf118fdec88dae82d946f37138
弋紫轩 <2019011307@secoder.net>
2021-04-08 12:26:06 +0800

    fix: add dev branch to deploy and fix more bugs on get

--- logIn/views.py
+++ logIn/views.py
@@ -132,12 +132,12 @@ def get_user_info(request):
                     'private_count':
                     user_info['private_count'],
                     'total_like_count':
-                    app.utils.get_total_like_count(request.Get.get('open_id')),
+                    app.utils.get_total_like_count(request.GET.get('open_id')),
                     'total_comment_total':
                     app.utils.get_total_comment_count(
                         request.Get.get('open_id')),
                     'total_view_count':
-                    app.utils.get_total_view_count(request.Get.get('open_id'))
+                    app.utils.get_total_view_count(request.GET.get('open_id'))
                 },
                 'open_id': request.Get.get('open_id')
             })
