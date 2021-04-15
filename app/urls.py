152fc760fe6697317e1c2eeb93a54226baa0b677
弋紫轩 <2019011307@secoder.net>
2021-04-15 20:40:36 +0800

    feat: implement the analyseFans module #25

--- app/urls.py
+++ app/urls.py
@@ -20,5 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('logIn.urls')),
     path('api/video/', include('manageWorks.urls')),
-    path('api/user/', include('analyseWorks.urls')),
+    path('api/analysis/', include('analyseWorks.urls', 'analyseFans.urls')),
 ]
