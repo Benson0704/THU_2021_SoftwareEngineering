cadb3c46e37416e87a10dd48d3dd436e0abe810f
Benson0704 <wang-bn19@mails.tsinghua.edu.cn>
2021-04-20 09:59:01 +0800

    fix: fix not found bugs

--- feedback/urls.py
+++ feedback/urls.py
@@ -7,6 +7,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    ''''
     path('single', views.get_videos_info_by_time),
     path('global_day', views.get_all_videos_info)
+    '''
 ]
