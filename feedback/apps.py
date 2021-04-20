3809cf44cee64cf2569eb91cca911d9413edcd81
弋紫轩 <2019011307@secoder.net>
2021-04-20 18:06:34 +0800

    style: fix style errors in share, notice and feedback module #34

--- feedback/apps.py
+++ feedback/apps.py
@@ -1,3 +1,6 @@
+'''
+this module should help setup app
+'''
 from django.apps import AppConfig
 
 
--- share/models.py
+++ share/models.py
@@ -1,3 +1,3 @@
-from django.db import models
-
-# Create your models here.
+'''
+this module should be a models file
+'''
