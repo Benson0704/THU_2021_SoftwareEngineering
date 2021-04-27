c7c689a2c2b2d8cc5c36f85b52457934c17f19fb
brisa <zhaoxiyu18@mails.tsinghua.edu.cn>
2021-04-27 13:11:21 +0800

    fix: add add_test url changes, #42

--- logIn/urls.py
+++ logIn/urls.py
@@ -8,5 +8,7 @@ from . import views
 
 urlpatterns = [
     path('home', views.get_user_info_by_code),
-    path('home/id', views.get_user_info_by_id)
+    path('home/id', views.get_user_info_by_id),
+    path('home/add',view.add_test)
+    path('home/delete',viwe.delete_test)
 ]
