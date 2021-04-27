3e007db19482e00f9685f544f6a9493502f5fa2b
brisa <zhaoxiyu18@mails.tsinghua.edu.cn>
2021-04-27 13:17:40 +0800

    fix: fix bugs in  add_test url changes, #42

--- logIn/urls.py
+++ logIn/urls.py
@@ -9,6 +9,6 @@ from . import views
 urlpatterns = [
     path('home', views.get_user_info_by_code),
     path('home/id', views.get_user_info_by_id),
-    path('home/add',view.add_test)
-    path('home/delete',viwe.delete_test)
+    path('home/add',views.add_test),
+    path('home/delete',views.delete_test)
 ]
