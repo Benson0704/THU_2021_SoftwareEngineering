31b4e249a1354eb697d390d3a013218fcf3694b7
弋紫轩 <2019011307@secoder.net>
2021-04-29 14:15:12 +0800

    style: delete unused import #45

--- logIn/views.py
+++ logIn/views.py
@@ -7,7 +7,7 @@ from django.http import HttpResponse
 import app.api
 import app.utils
 import app.times
-from app.models import User, Request, Performance
+from app.models import User
 from apscheduler.schedulers.background import BackgroundScheduler
 from django_apscheduler.jobstores import DjangoJobStore, \
     register_job, register_events
