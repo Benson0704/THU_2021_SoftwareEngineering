6511cf0b00da1001816f8c3a5fd814114889e063
Benson0704 <wang-bn19@mails.tsinghua.edu.cn>
2021-05-08 04:44:04 +0800

    fix: locate negative delta bugs in user databases and fix it #66

--- analyseWorks/views.py
+++ analyseWorks/views.py
@@ -210,9 +210,13 @@ def get_all_videos_info(request):
                             'comment_count'] -= analyse.total_comment_count
                         break
             if today == 1 and len(res_list) == 1:
-                res_list[0]['like_count'] = user.total_like_count
-                res_list[0]['view_count'] = user.total_view_count
-                res_list[0]['comment_count'] = user.total_comment_count
+                res_list[0]['like_count'] = app.utils.get_total_like_count(
+                    open_id)
+                res_list[0]['view_count'] = app.utils.get_total_view_count(
+                    open_id)
+                res_list[0][
+                    'comment_count'] = app.utils.get_total_comment_count(
+                        open_id)
                 res_list[0]['video_count'] = 0
                 for video in video_list:
                     analyses = video.analysis.all().order_by('sum_time')
@@ -220,11 +224,11 @@ def get_all_videos_info(request):
                         if app.times.datetime2timestamp(
                                 analyse.sum_time) == begin_timestamp:
                             res_list[0][
-                                'like_count'] -= -analyse.total_like_count
+                                'like_count'] -= analyse.total_like_count
                             res_list[0][
-                                'view_count'] -= -analyse.total_view_count
+                                'view_count'] -= analyse.total_view_count
                             res_list[0][
-                                'comment_count'] -= -analyse.total_comment_count
+                                'comment_count'] -= analyse.total_comment_count
                     if app.times.datetime2timestamp(
                             video.create_time) > term_timestamp + 1 - 86400:
                         res_list[0]['video_count'] += 1
@@ -263,9 +267,13 @@ def get_all_videos_info(request):
                                  86400]['video_count'] += 1
                 begin += 86400
             if today == 1:
-                res_list[-1]['like_count'] = user.total_like_count
-                res_list[-1]['view_count'] = user.total_view_count
-                res_list[-1]['comment_count'] = user.total_comment_count
+                res_list[-1]['like_count'] = app.utils.get_total_like_count(
+                    open_id)
+                res_list[-1]['view_count'] = app.utils.get_total_view_count(
+                    open_id)
+                res_list[-1][
+                    'comment_count'] = app.utils.get_total_comment_count(
+                        open_id)
                 for video in video_list:
                     analyses = video.analysis.all().order_by('-sum_time')
                     for i, analyse in enumerate(analyses):
